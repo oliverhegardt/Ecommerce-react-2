2336636b32534e31c399f2ba86385b39c239ce2d
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-06-08 15:41:24 +0200

    Update CategoryItem.jsx

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,9 +1,32 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+`;
+const Title = styled.h1``;
+const Button = styled.button``;
 
 function CategoryItem({ item }) {
-  return <Container>Hello</Container>;
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  );
 }
 
 export default CategoryItem;
