20364b70136567d06c79b1580172a1b929bd2f2f
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-06-09 21:19:43 +0200

    Creating Categories

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Container = styled.div`
   flex: 1;
   margin: 3px;
-  height: 70vh;
+  /* height: 70vh; */
   position: relative;
 `;
 const Image = styled.img`
@@ -13,9 +13,28 @@ const Image = styled.img`
 `;
 const Info = styled.div`
   position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
-const Title = styled.h1``;
-const Button = styled.button``;
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-weight: 600;
+  border-radius: 25px;
+`;
 
 function CategoryItem({ item }) {
   return (
---
+++ src/components/Product.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Product = (item) => {
+  return <Container>Product</Container>;
+};
+
+export default Product;
