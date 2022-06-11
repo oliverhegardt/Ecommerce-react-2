5715952c94a61031dc99938078ffdcb670d101dd
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-06-11 09:45:05 +0200

    Updates products

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Container = styled.div`
   flex: 1;
   margin: 3px;
-  /* height: 70vh; */
+  height: 70vh;
   position: relative;
 `;
 const Image = styled.img`
