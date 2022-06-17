9c2a0f47d844d89321b3b5712da01a922b689e4e
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-06-17 22:09:51 +0200

    Create Newsletter.jsx

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Newsletter = () => {
+    return (
+        <Container>
+            <Title></Title>
+            <Description></Description>
+            <InputContainer></InputContainer>
+        </Container>
+    );
+};
+
+export default Newsletter;
