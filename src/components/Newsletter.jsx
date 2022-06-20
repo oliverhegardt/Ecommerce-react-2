a576b1ce8b3dc6e467b29fad64d03fd14ca1ecd8
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-06-20 21:40:04 +0200

    Working on footer

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -26,8 +26,17 @@ const InputContainer = styled.div`
     justify-content: space-between;
     border: 1px solid lightgray;
 `;
-const Input = styled.input``;
-const Button = styled.button``;
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+`;
+const Button = styled.button`
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: white;
+`;
 
 const Newsletter = () => {
     return (
