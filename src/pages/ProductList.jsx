2cfbb3e381b91548a75643a257b4a10c80649ebb
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-08-12 10:50:21 +0200

    Update ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import Announcment from "../components/Announcement";
 import Navbar from "../components/Navbar";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Containter = styled.div``;
 const Title = styled.h1`
@@ -14,6 +17,13 @@ const Filter = styled.div`
     margin: 20px;
 `;
 
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+`;
+const Select = styled.select``;
+const Option = styled.option``;
+
 const ProductList = () => {
     return (
         <Containter>
@@ -21,9 +31,25 @@ const ProductList = () => {
             <Navbar />
             <Title>Dresses</Title>
             <FilterContainer>
-                <Filter>filter</Filter>
-                <Filter>filter2</Filter>
+                <Filter>
+                    <FilterText>Filter Prodcuts</FilterText>
+                    <Select>
+                        <Option disabled>Color</Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                </Filter>
+                <Filter>
+                    <FilterText>Sort Products</FilterText>
+                </Filter>
             </FilterContainer>
+            <Products />
+            <Newsletter />
+            <Footer />
         </Containter>
     );
 };
