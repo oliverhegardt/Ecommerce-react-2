611edcb348004248271d3eea790de89bb17d5b44
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-08-13 17:52:27 +0200

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,11 +4,18 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 
-const Containter = styled.div``;
+const Container = styled.div``;
+const Wrapper = styled.div``;
+const ImgContainer = styled.div``;
+const Image = styled.img``;
+const InfoContainer = styled.div``;
+const Title = styled.h1``;
+const Desc = styled.p``;
+const Price = styled.span``;
 
 const Product = () => {
     return (
-        <Containter>
+        <Container>
             <Announcement />
             <Navbar />
             <Wrapper>
@@ -16,14 +23,19 @@ const Product = () => {
                     <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
                 </ImgContainer>
                 <InfoContainer>
-                    <Title></Title>
-                    <Description></Description>
+                    <Title>Denim Jumpsuit </Title>
+                    <Desc>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Donec tempor dapibus ipsum, vitae porttitor metus
+                        lacinia in. Class aptent taciti sociosqu ad litora
+                        torquent per conubia nostra, per inceptos himenaeos.{" "}
+                    </Desc>
                     <Price> 20€</Price>
                 </InfoContainer>
             </Wrapper>
             <Newsletter />
             <Footer />
-        </Containter>
+        </Container>
     );
 };
 
