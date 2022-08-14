b26dda31b7df56a5ce91661e81ca52dc10b1aab6
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-08-14 17:29:40 +0200

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,13 +5,65 @@ import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.img``;
-const InfoContainer = styled.div``;
-const Title = styled.h1``;
-const Desc = styled.p``;
-const Price = styled.span``;
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+`;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const Price = styled.span`
+    font-wight: 100;
+    font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+`;
+
+const FilterSize = styled.select``;
+
+const FilterSizeOption = styled.option``;
 
 const Product = () => {
     return (
@@ -28,9 +80,27 @@ const Product = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Donec tempor dapibus ipsum, vitae porttitor metus
                         lacinia in. Class aptent taciti sociosqu ad litora
-                        torquent per conubia nostra, per inceptos himenaeos.{" "}
+                        torquent per conubia nostra, per inceptos himenaeos.
                     </Desc>
                     <Price> 20€</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black"></FilterColor>
+                            <FilterColor color="darkblue"></FilterColor>
+                            <FilterColor color="gray"></FilterColor>
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
                 </InfoContainer>
             </Wrapper>
             <Newsletter />
