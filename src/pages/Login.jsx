8200743ceae41cece4b2d735b93ab67a56ace64d
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-08-17 15:39:08 +0200

    Updates Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,86 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+            rgba(255, 255, 255, 0.5),
+            rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+            center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: white;
+    border-radius: 10px;
+`;
+
+const Title = styled.h1`
+    margin-bottom: 20px;
+    font-size: 24px;
+    font-weight: 300;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 45%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+    border-radius: 10px;
+    border-width: 1px;
+`;
+
+const ButtonContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Button = styled.button`
+    border-radius: 10px;
+    width: 20%;
+    border: none;
+    padding: 15px 0px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
+
+const Link = styled.a``;
 
 const Login = () => {
-    return <Container>Hej</Container>;
+    return (
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input placeholder="Username" />
+
+                    <Input placeholder="Password" />
+
+                    <ButtonContainer>
+                        <Button>LOGIN</Button>
+                    </ButtonContainer>
+                    <Link>FORGOT PASSWORD?</Link>
+                    <Link>CREATE NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -35,7 +35,7 @@ const Form = styled.form`
 
 const Input = styled.input`
     flex: 1;
-    min-width: 40%;
+    min-width: 45%;
     margin: 20px 10px 0px 0px;
     padding: 10px;
     border-radius: 10px;
@@ -62,6 +62,7 @@ const Button = styled.button`
     padding: 15px 0px;
     background-color: teal;
     color: white;
+    cursor: pointer;
 `;
 
 const Register = () => {
