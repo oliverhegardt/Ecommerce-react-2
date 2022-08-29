f91358b7de4386442e3d05fd99373fbc1bb8036a
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-08-29 13:01:33 +0200

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -14,9 +14,42 @@ const Title = styled.h1`
     text-align: center;
 `;
 
-const Top = styled.div``;
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`;
 
-const Bottom = styled.div``;
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${(props) => props.type === "filled" && "none"};
+    background-color: ${(props) =>
+        props.type === "filled" ? "black" : "transparent"};
+    color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``;
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+`;
+
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Info = styled.div`
+    flex: 3;
+`;
+
+const Summary = styled.div`
+    flex: 1;
+`;
 
 const Cart = () => {
     return (
@@ -25,8 +58,25 @@ const Cart = () => {
             <Announcement />
             <Wrapper>
                 <Title>MY CART</Title>
-                <Top></Top>
-                <Bottom></Bottom>
+                <Top>
+                    <TopButton>CONTINUE SHOPPING</TopButton>
+                    <TopTexts>
+                        <TopText> Shopping Bag (2)</TopText>
+                        <TopText>Your Wishlist (0)</TopText>
+                    </TopTexts>
+                    <TopButton type="filled">CHECKOUT NOW</TopButton>
+                </Top>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
+                            </ProductDetail>
+                            <PriceDetail></PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>summary</Summary>
+                </Bottom>
             </Wrapper>
 
             <Footer />
