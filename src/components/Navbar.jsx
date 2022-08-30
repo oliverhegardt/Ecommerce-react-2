b64605956a66919cd99bb8a1fdbf6a78e712e426
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-08-30 17:04:42 +0200

    Cart done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,88 +4,88 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 60px;
+    height: 60px;
 `;
 
 const Wrapper = styled.div`
-  padding: 10px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `;
 
 const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
+    flex: 1;
+    display: flex;
+    align-items: center;
 `;
 
 const Language = styled.span`
-  font-size: 14px;
-  cursor: pointer;
+    font-size: 14px;
+    cursor: pointer;
 `;
 
 const SearchContainer = styled.div`
-  border: 1px solid lightgrey;
-  display: flex;
-  align-items: center;
-  margin-left: 25px;
-  padding: 5px;
-  border-radius: 25px;
+    border: 1px solid lightgrey;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+    border-radius: 25px;
 `;
 
 const Input = styled.input`
-  border: none;
+    border: none;
 `;
 
 const Center = styled.div`
-  flex: 1;
-  text-align: center;
+    flex: 1;
+    text-align: center;
 `;
 
 const Logo = styled.h1`
-  font-weight: bold;
+    font-weight: bold;
 `;
 const Right = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `;
 
 const MenuItem = styled.div`
-  font-size: 14px;
-  cursor: pointer;
-  margin-left: 25px;
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
 `;
 
 const Navbar = () => {
-  return (
-    <Container>
-      <Wrapper>
-        <Left>
-          <Language>EN</Language>
-          <SearchContainer>
-            <Input />
-            <Search style={{ color: "gray", fontSize: 16 }} />
-          </SearchContainer>
-        </Left>
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input />
+                        <Search style={{ color: "gray", fontSize: 16 }} />
+                    </SearchContainer>
+                </Left>
 
-        <Center>
-          <Logo>OLIVES.</Logo>
-        </Center>
-        <Right>
-          <MenuItem>REGISTER</MenuItem>
-          <MenuItem>SIGN IN</MenuItem>
-          <MenuItem>
-            <Badge badgeContent={4} color="primary">
-              <ShoppingCartOutlined />
-            </Badge>
-          </MenuItem>
-        </Right>
-      </Wrapper>
-    </Container>
-  );
+                <Center>
+                    <Logo>OLIVES.</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
+    );
 };
 
 export default Navbar;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -25,6 +25,7 @@ const Top = styled.div`
 const TopButton = styled.button`
     padding: 10px;
     font-weight: 600;
+    border-radius: 10px;
     cursor: pointer;
     border: ${(props) => props.type === "filled" && "none"};
     background-color: ${(props) =>
@@ -114,27 +115,40 @@ const Hr = styled.hr`
 
 const Summary = styled.div`
     flex: 1;
+    border: 0.5px solid lightgrey;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
 `;
 
 const SummaryTitle = styled.h1`
-    font-size: 28px;
     font-weight: 200;
-    margin: 20px;
+    text-align: center;
 `;
 
 const SummaryItem = styled.div`
-    margin: 20px;
+    margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    font-weight: ${(props) => props.type === "total" && "500"};
+    font-size: ${(props) => props.type === "total" && "24px"};
 `;
 
-const SummaryItemText = styled.span`
-    font-size: ${(props) => props.total};
-`;
+const SummaryItemText = styled.span``;
 
 const SummaryItemPrice = styled.span``;
 
-const Button = styled.button``;
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    font-weight: 600;
+
+    cursor: pointer;
+    border: none;
+    border-radius: 10px;
+    background-color: black;
+    color: white;
+`;
 
 const Cart = () => {
     return (
@@ -223,10 +237,8 @@ const Cart = () => {
                             <SummaryItemText>Shipping Discount</SummaryItemText>
                             <SummaryItemPrice>- $ 5.90</SummaryItemPrice>
                         </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText type="total">
-                                Total
-                            </SummaryItemText>
+                        <SummaryItem type="total">
+                            <SummaryItemText>Total</SummaryItemText>
                             <SummaryItemPrice>$ 300.0</SummaryItemPrice>
                         </SummaryItem>
                         <Button>CHECKOUT NOW</Button>
